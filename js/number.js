70423aaccad1ca7a6497f3e1132a1e0c642bb98e
SlavikUA <slavikair2020@gmail.com>
2021-07-25 21:24:06 +0300

    add conversion block

--- js/number.js
+++ js/number.js
@@ -3,14 +3,30 @@ let counteNumber = document.querySelector('.counter-number');
 
 let counter = 0;
 
+let counterV = 0;
 
-let timeId = setInterval( ()=> {
+
+let timeIdNumber = setInterval( ()=> {
 
     counteNumber.innerHTML = counter;
-
     counter++;
 
     if ( counter >= 1695) {
-        clearTimeout(timeId);
+        clearTimeout(timeIdNumber);
     }
 }, 10);
+
+let counterVisitors = document.querySelector('.counter-visitors');
+
+let timeIdVisitors = setInterval ( ()=> {
+
+    counterVisitors.innerHTML = counterV;
+    counterV++;
+
+    if ( counterV >= 960) {
+        clearTimeout(timeIdVisitors);
+    }
+
+}, 10);
+
+
